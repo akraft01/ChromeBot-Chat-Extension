8a5a6c1cc936fe2b5f5f1b6791332f310271e334
akraft01 <71739202+akraft01@users.noreply.github.com>
2023-11-12 17:08:25 -0500

    Update content.js

--- src/content/content.js
+++ src/content/content.js
@@ -1,1 +1,25 @@
+// content/content.js
+
+
+function sendMessageToBackground() {
+  const message = {
+    type: 'someEvent',
+    data: 'Some data related to the event',
+  };
+
+  // Send the message to the background script
+  chrome.runtime.sendMessage(message, (response) => {
+    console.log('Received response from background script:', response);
+  });
+}
+
+// function to modify the content of the page
+function modifyPageContent() {
+  
+  console.log('Modifying page content');
+}
+
+// Call the functions when the content script is injected into a page
+sendMessageToBackground();
+modifyPageContent();
 
