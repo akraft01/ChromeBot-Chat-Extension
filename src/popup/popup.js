13a2dcab12e45e448368dd40826b4a3bcbfb4a05
akraft01 <71739202+akraft01@users.noreply.github.com>
2023-11-12 17:10:37 -0500

    Update popup.js

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,1 +1,15 @@
+// popup/popup.js
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Code to run when the popup HTML has been fully loaded
+
+  // Add a click event listener to the button
+  const popupButton = document.getElementById('popupButton');
+  popupButton.addEventListener('click', function () {
+    // Send a message to the background script when the button is clicked
+    chrome.runtime.sendMessage({ type: 'popupButtonClick' }, function (response) {
+      console.log('Received response from background script:', response);
+    });
+  });
+});
 
